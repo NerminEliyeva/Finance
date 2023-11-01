9f36cbd5a6550f319eef4b911ce8674dc0cce78e
NerminEliyeva <narminahmedova1997@gmail.com>
2023-11-02 03:01:42 +0400

    serilog and global exception handling middleware

---
+++ Finance.Api/Extentions/AppExtentions.cs
@@ -0,0 +1,39 @@
+﻿using Serilog;
+using System.Diagnostics;
+
+namespace Finance.Api.Extentions
+{
+    public static class AppExtentions
+    {
+        public static void AddMyLogger(this ILoggingBuilder logging, WebApplicationBuilder builder)
+        {
+            #region 
+            // Hrded code loglevel
+            //builder.Logging.AddFilter<ConsoleLoggerProvider>("Microsoft", LogLevel.Warning);
+
+            //// Hard Codded
+            //var hardCodedLogger = new LoggerConfiguration()
+            //    .MinimumLevel.Information()
+            //    .MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Warning)
+            //    .MinimumLevel.Override("System", Serilog.Events.LogEventLevel.Warning)
+            //    .WriteTo.Console()
+            //    .WriteTo.File(path: "./DemoApilog-.txt", rollingInterval: RollingInterval.Day)
+            //    .CreateLogger();
+            //builder.Logging.ClearProviders();
+            #endregion
+
+            // Added Serilog from file
+            var fromFile = new LoggerConfiguration()
+                            .ReadFrom.Configuration(builder.Configuration)
+                            .CreateLogger();
+
+
+            logging.AddSerilog(fromFile);
+            Serilog.Debugging.SelfLog.Enable(msg =>
+            {
+                Debug.Print(msg);
+                Debugger.Break();
+            });
+        }
+    }
+}
---
+++ Finance.Application/Response/ResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Finance.Application.Response
+{
+    public class ResponseModel
+    {
+        public int responseCode { get; set; }
+        public string? responseMessage { get; set; }
+    }
+}
